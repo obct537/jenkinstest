fb65368ca23379bd209718b2520bbf6a6161d9cc
Sam Schwartz <samschwa@cisco.com>
2019-04-11 09:45:56 -0400

    maybe?

--- uploader.groovy
+++ uploader.groovy
@@ -1,5 +1,7 @@
+import groovy.json.JsonOutput
+
 def upload(pw) {
-    def json_stuff = "\"{\"productName\": \"Test Product Name 44\", \"username\": \"samschwa\", \"password\":\"" + pw + "\", \"images\": [\"thing.txt\", \"images/*.txt\"]}\""
+    def json_stuff = JsonOutput.toJson([productName: "Test Product Name 44", username: "samschwa", password: pw, images: ["thing.txt", "images/*.txt"]])
 
     println json_stuff
     def command = "python3 /imageuploader/coronaApiHandler.py --json "
