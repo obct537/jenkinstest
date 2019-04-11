ec98088998f9ad6bf16e55d218133b7927ff684f
Sam Schwartz <samschwa@cisco.com>
2019-04-11 09:38:17 -0400

    ?

--- uploader.groovy
+++ uploader.groovy
@@ -1,10 +1,9 @@
 def upload(pw) {
-    def json_stuff = "{\"productName\": \"Test Product Name 44\", \"username\": \"samschwa\", \"password\":\"" + pw + "\", \"images\": [\"thing.txt\", \"images/*.txt\"]}"
+    def json_stuff = "\"{\"productName\": \"Test Product Name 44\", \"username\": \"samschwa\", \"password\":\"" + pw + "\", \"images\": [\"thing.txt\", \"images/*.txt\"]}\""
 
     println json_stuff
-    def command = 'python3 /imageuploader/coronaApiHandler.py --json "' 
+    def command = 'python3 /imageuploader/coronaApiHandler.py --json ' 
     command += json_stuff
-    command += '"'
 
     println command
 
