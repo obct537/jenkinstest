e919eca6b7a077f2999f6c1ef709a4154e42a197
Sam Schwartz <samschwa@cisco.com>
2019-04-11 09:47:18 -0400

    idfk

--- uploader.groovy
+++ uploader.groovy
@@ -4,8 +4,7 @@ def upload(pw) {
     def json_stuff = JsonOutput.toJson([productName: "Test Product Name 44", username: "samschwa", password: pw, images: ["thing.txt", "images/*.txt"]])
 
     println json_stuff
-    def command = "python3 /imageuploader/coronaApiHandler.py --json "
-    command += json_stuff
+    def command = "python3 /imageuploader/coronaApiHandler.py --json $json_stuff"
 
     println command
 
