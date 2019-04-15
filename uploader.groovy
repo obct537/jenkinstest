1cc3aec5a198c32f6ff8b3e6b0ca31396d94ee73
Sam Schwartz <samschwa@cisco.com>
2019-04-15 08:43:42 -0400

    debugging

--- uploader.groovy
+++ uploader.groovy
@@ -4,6 +4,13 @@ def upload(creds) {
     def pw = creds.split(':')[1]
     def json_stuff = JsonOutput.toJson([directory: pwd(), version: '111', productName: "Test Product Name 44", username: "samschwa", password: pw, images: ["thing.txt", "images/*.txt"]])
 
+    def testthing = [itemA: 'a', itemB: 'b']
+    
+    if( testthing.containsKey('itemA') ) {
+        println 'a'
+    } else {
+        println 'b'
+    }
     def command = "python3 /imageuploader/coronaApiHandler.py --json '$json_stuff'"
 
     println command
